938751c853a1f2a18f75c3ad5b235af26e63c5bc
divya-atpar <divya@atparui.com>
2025-06-22 10:13:58 +0530

    Add GSAP animations and dependencies
    - Introduced GSAP animations in multiple components: GsapFrom, GsapFromTo, GsapScrollTrigger, GsapStagger, GsapText, GsapTimeline, and GsapTo.
    - Implemented various GSAP methods including from, fromTo, to, and timeline for animations.
    - Added GSAP and @gsap/react as dependencies in package.json.
    - Created a new lockfile (bun.lock) to manage dependencies.

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,5 +1,18 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
+
+  useGSAP(() => {
+    gsap.to("#blue-box", {
+      x: 250,
+      duration: 2,
+      ease: "elastic.out(1, 0.3)",
+      yoyo: true,
+      repeat: -1,
+    });
+  }, []);
 
   return (
     <main>
